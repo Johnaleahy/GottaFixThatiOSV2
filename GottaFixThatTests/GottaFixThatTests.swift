c74ac6d430a4c2c413f861fd24070f37d2b0a941
John Leahy with Claude <jleahy@immediac.com>
2026-01-18 07:09:18 -0400

    Initial Commit

---
+++ GottaFixThatTests/GottaFixThatTests.swift
@@ -0,0 +1,17 @@
+//
+//  GottaFixThatTests.swift
+//  GottaFixThatTests
+//
+//  Created by John Leahy on 2026-01-18.
+//
+
+import Testing
+@testable import GottaFixThat
+
+struct GottaFixThatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
